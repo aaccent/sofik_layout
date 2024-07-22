e6ba92940fc7908d5e67f446a8a69ec5f920af16
foxhable <foxhable@gmail.com>
2024-07-22 15:55:21 +0300

    feat: contacts-form

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -12,5 +12,6 @@ import '@/components/our-projects/our-projects.js'
 import '@/components/seo/seo.js'
 import '@/components/FAQ/FAQ.js'
 import '@/components/specialists/specialists.js'
+import '@/components/contacts-form/contacts-form.js'
 
 import '@/ui/file-input/file-input.js'
