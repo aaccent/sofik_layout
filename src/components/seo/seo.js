942414628ef77cc6389329d4bbd41acad4359789
Regina <rmyachina@mail.ru>
2024-07-22 14:19:56 +0300

    feat: about-page

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -1,9 +1,12 @@
-const seoText = document.querySelector('.seo__text')
-const moreBtn = document.querySelector('.seo__button')
+void (function () {
+    const seoText = document.querySelector('.seo__text')
+    if (!seoText) return
+    const moreBtn = document.querySelector('.seo__button')
 
-moreBtn.addEventListener('click', () => {
-    seoText.classList.toggle('_opened')
-    seoText.classList.contains('_opened')
-        ? (moreBtn.textContent = 'Скрыть')
-        : (moreBtn.textContent = 'Читать полностью')
-})
+    moreBtn.addEventListener('click', () => {
+        seoText.classList.toggle('_opened')
+        seoText.classList.contains('_opened')
+            ? (moreBtn.textContent = 'Скрыть')
+            : (moreBtn.textContent = 'Читать полностью')
+    })
+})()
