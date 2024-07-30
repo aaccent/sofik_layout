59ccacf1a4fa3adafa5839d208e7a5208b7d9c64
Regina <rmyachina@mail.ru>
2024-07-30 15:36:57 +0300

    feat: calculator functionality

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,63 +1,104 @@
 import IMask from 'imask'
+import AirDatepicker from 'air-datepicker'
+import { suggestionPluralDay, suggestionPluralRubles } from 'features/pluralRules.ts'
+
+document.querySelectorAll('input').forEach((input) => input.setAttribute('autocomplete', 'off'))
+let amount
+let period
+let rate = 0.015
+
+function showResult() {
+    const resultOutput = document.querySelector('.calculator__field-output')
+
+    if (!amount || !period) {
+        resultOutput.value = '0 ₽'
+    } else {
+        const result = Math.round(((amount * rate) / 365) * period).toLocaleString('ru')
+        if (result < 750) {
+            resultOutput.value = `750 ₽`
+        } else {
+            resultOutput.value = `${result} ₽`
+        }
+    }
+}
 
 void (function () {
     const fields = document.querySelectorAll('.field:has(input)')
     if (!fields.length) return
     fields.forEach((field) => {
-        field.addEventListener('click', () => {
+        field.addEventListener('click', (e) => {
             const input = field.querySelector('input')
+            if (e.target.classList.contains('pseudo')) return
             field.classList.add('_active')
             input.focus()
             input.onblur = () => {
                 if (input.value) return
                 field.classList.remove('_active')
             }
-
-            input.oninput = () => {}
         })
     })
+})()
 
-    const amountInput = document.querySelector('._amount input')
-    IMask(amountInput, {
-        mask: 'num ₽',
-        lazy: false,
+void (function () {
+    const amountInput = document.querySelector('.field._amount input')
+    const periodInput = document.querySelector('.field._period input')
 
-        blocks: {
-            num: {
-                mask: Number,
-                thousandsSeparator: ' ',
-                max: 999999999999999999999,
-            },
+    const amountMasked = IMask(amountInput, {
+        mask: Number,
+        thousandsSeparator: ' ',
+    })
+
+    const periodMasked = IMask(periodInput, {
+        mask: Number,
+    })
+
+    amountInput.addEventListener('blur', () => {
+        if (amountInput.value) {
+            amountInput.value += ` ${suggestionPluralRubles.get(Number(amountMasked.unmaskedValue))}`
+            amount = Number(amountMasked.unmaskedValue)
+            showResult()
+        }
+    })
+
+    periodInput.addEventListener('blur', () => {
+        if (periodInput.value) {
+            periodInput.value += ` ${suggestionPluralDay.get(Number(periodMasked.unmaskedValue))}`
+            period = Number(periodMasked.unmaskedValue)
+            showResult()
+        }
+    })
+})()
+
+function showPeriod(arr, input) {
+    if (arr.length !== 2) return
+    const start = new Date(arr[0].toDateString())
+    const end = new Date(arr[1].toDateString())
+    const periodLength = Math.abs(end - start) / 86400000
+    input.value = `${periodLength} ${suggestionPluralDay.get(periodLength)}`
+    period = periodLength
+    showResult()
+}
+
+void (function () {
+    const field = document.querySelector('.field._period')
+    const input = field.querySelector('input')
+
+    const pseudoInput = document.createElement('input')
+    pseudoInput.classList.add('pseudo')
+    field.append(pseudoInput)
+
+    new AirDatepicker(pseudoInput, {
+        range: true,
+        onSelect: ({ date }) => {
+            showPeriod(date, input)
         },
     })
 })()
 
 void (function () {
-    const dateInput = document.querySelector('input[type="date"]')
-    if (!dateInput) return
-    const field = dateInput.closest('._period')
-    const periodOutput = field.querySelector('.field__input')
-
-    const now = new Date()
-    const nowYear = now.getFullYear()
-    const nowMonth = String(now.getMonth() + 1).length < 2 ? `0${now.getMonth() + 1}` : now.getMonth() + 1
-    const nowDay = String(now.getDate()).length < 2 ? `0${now.getDate()}` : now.getDate()
-    const today = `${nowYear}-${nowMonth}-${nowDay}`
-
-    dateInput.setAttribute('min', today)
-
-    dateInput.oninput = () => {
-        const nowTimestamp = now.getTime()
-        const resultTimestamp = new Date(dateInput.value).getTime()
-        const periodTimestamp = resultTimestamp - nowTimestamp
-        field.classList.add('_active')
-        periodOutput.value = Math.round(periodTimestamp / 86400000)
-    }
-})()
-
-void (function () {
     const selectors = document.querySelectorAll('.calculator__selector')
     if (!selectors.length) return
+
     selectors.forEach((selector) => {
         selector.addEventListener('click', () => {
             const activeSelector = document.querySelector('.calculator__selector._active')
@@ -70,11 +111,21 @@ void (function () {
 
     const options = document.querySelectorAll('.calculator__selector-options-item')
     options.forEach((option) => {
+        const rateOutput = document.querySelector('.calculator__field-rate')
         option.addEventListener('click', () => {
             const selected = option.closest('.calculator__selector').querySelector('.calculator__selector-selected')
             const hiddenInput = option.closest('.calculator__selector-options').querySelector('input[type="hidden"]')
             selected.textContent = option.textContent
             hiddenInput.value = option.textContent
+            if (option.textContent === 'Коммерческий контракт') {
+                rateOutput.textContent = '3% годовых'
+                rate = 0.03
+                showResult()
+            } else {
+                rateOutput.textContent = '1.5% годовых'
+                rate = 0.015
+                showResult()
+            }
         })
     })
 })()
