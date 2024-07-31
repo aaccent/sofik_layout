1064dcd05923628f48f382933aac8f62436d3ff7
Regina <rmyachina@mail.ru>
2024-07-31 14:23:08 +0300

    feat: pagination bullets animation

--- src/components/main-hero/main-hero.js
+++ src/components/main-hero/main-hero.js
@@ -13,4 +13,25 @@ new Swiper('.main-hero .swiper', {
     slidesPerView: 1,
     effect: 'fade',
     crossFade: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    on: {
+        init: () => {
+            const bullets = document.querySelectorAll('.swiper-pagination-bullet')
+            bullets.forEach((bullet) => {
+                const span = document.createElement('span')
+                span.innerHTML = `<svg viewBox="0 0 48 48">
+          <circle cx="24" cy="24" r="20" class="loader"></circle>
+        </svg>`
+                bullet.append(span)
+            })
+        },
+        autoplayTimeLeft(s, time, progress) {
+            const bullet = document.querySelector('.swiper-pagination-bullet')
+            bullet.style.setProperty('--time', time / 1000)
+        },
+    },
 })
