9bab77bc19e0a505ec7a6f5d224e2d7ed673100b
foxhable <foxhable@gmail.com>
2024-08-07 11:59:54 +0300

    refactor: change call popup input names

--- src/ui/popups/call-popup/call-popup.js
+++ src/ui/popups/call-popup/call-popup.js
@@ -11,25 +11,25 @@ void (function () {
         const amount = document.createElement('input')
         const amountInput = document.querySelector('input[name="amount"]')
         amount.setAttribute('type', 'hidden')
-        amount.setAttribute('name', amountInput.name || 'calc-amount')
+        amount.setAttribute('name', 'calc-amount')
         amount.value = amountInput.value
 
         const period = document.createElement('input')
         const periodInput = document.querySelector('input[name="period"]')
         period.setAttribute('type', 'hidden')
-        period.setAttribute('name', periodInput.name || 'calc-period')
+        period.setAttribute('name', 'calc-period')
         period.value = periodInput.value
 
         const rate = document.createElement('input')
         const rateInput = document.querySelector('.calculator__field-rate')
         rate.setAttribute('type', 'hidden')
-        rate.setAttribute('name', rateInput.name || 'calc-rate')
+        rate.setAttribute('name', 'calc-rate')
         rate.value = rateInput.value
 
         const result = document.createElement('input')
         const resultInput = document.querySelector('.calculator__field-output')
         result.setAttribute('type', 'hidden')
-        result.setAttribute('name', resultInput.name || 'calc-result')
+        result.setAttribute('name', 'calc-result')
         result.value = resultInput.value
 
         popupForm.append(amount, period, rate, result)
